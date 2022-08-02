d9707b304f164c89fa0ad10ee19c60fb1d3a9d1c
mgintoki <904448980@qq.com>
2022-08-02 10:42:45 +0800

    Add grpc server module

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tristan-club/kit
 go 1.16
 
 require (
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/rs/zerolog v1.27.0
 	github.com/trustwallet/go-primitives v0.0.55
 	google.golang.org/grpc v1.48.0
---
+++ grpc/server/server.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	"github.com/tristan-club/kit/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+	"net"
+	"runtime/debug"
+)
+
+var (
+	customFunc grpc_recovery.RecoveryHandlerFuncContext
+)
+
+var GrpcServer *grpc.Server
+
+func Init() *grpc.Server {
+
+	// Define customfunc to handle panic
+	//Todo panic error
+	customFunc = func(ctx context.Context, p interface{}) error {
+		log.Error().Msgf("[PANIC] %s\n\n%s", p, string(debug.Stack()))
+		return status.Errorf(codes.Unknown, "panic triggered: %v", p)
+	}
+
+	opts := []grpc_recovery.Option{
+		grpc_recovery.WithRecoveryHandlerContext(customFunc),
+	}
+
+	// Create a server. Recovery handlers should typically be last in the chain_info so that other middleware
+	// (e.g. logging) can operate on the recovered state instead of being directly affected by any panic
+
+	grpcServer := grpc.NewServer(
+		grpc_middleware.WithUnaryServerChain(
+			grpc_recovery.UnaryServerInterceptor(opts...),
+			//otgrpc.OpenTracingServerInterceptor(thisTracer),
+		),
+		grpc_middleware.WithStreamServerChain(
+			grpc_recovery.StreamServerInterceptor(opts...),
+			//grpc_opentracing.StreamServerInterceptor(topts...),
+		),
+	)
+
+	return grpcServer
+}
+
+func Start(s *grpc.Server, port string) error {
+	grpcAddr := fmt.Sprintf("0.0.0.0:%s", port)
+	lis, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		return err
+	}
+
+	reflection.Register(s)
+	log.Info().Msgf("start grpc server at", grpcAddr)
+
+	return s.Serve(lis)
+}
